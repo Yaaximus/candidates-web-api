06f0f3869f82d49c702fd1cf58d0b258e21a4edd
Yaaximus <yasim.ahmed63@yahoo.com>
2020-06-17 01:03:05 +0500

    Frontend side code is under progress

--- api_requests.py
+++ api_requests.py
@@ -1,20 +1,20 @@
-# importing the requests library 
+# importing the requests library
 import requests
 import argparse
 
-# api-endpoint 
-URL = "http://127.0.0.1:5000/"
+# api-endpoint
+URL = "http://127.0.0.1:3000/"
 
 
 def test_candidatelist_get_method():
 
     complete_url = URL + "candidates_list/"
-    
-    # sending get request and saving the response as response object 
-    r = requests.get(url = complete_url)
 
-    # extracting data in json format 
-    data = r.json() 
+    # sending get request and saving the response as response object
+    r = requests.get(url=complete_url)
+
+    # extracting data in json format
+    data = r.json()
 
     for el in data:
 
@@ -25,21 +25,21 @@ def test_candidatelist_get_method():
 
 
 def test_candidatelist_post_method():
-    
-    # data to be sent to api 
-    data = {'name':'Yasim', 
-            'age':'24', 
-            'language':'python'} 
-    
-    complete_url = URL + "candidates_list/"
-    
-    # sending post request and saving response as response object 
-    r = requests.post(url = complete_url, data = data) 
-    
-    # extracting response text  
-    data = r.text 
 
-    print("New user added, Details are:%s"%data)
+    # data to be sent to api
+    data = {'name': 'Ahmad',
+            'age': '24',
+            'language': 'python'}
+
+    complete_url = URL + "candidates_list/"
+
+    # sending post request and saving response as response object
+    r = requests.post(url=complete_url, data=data)
+
+    # extracting response text
+    data = r.text
+
+    print("New user added, Details are:%s" % data)
 
 
 def test_candidate_get_method(candidate_id):
@@ -48,10 +48,10 @@ def test_candidate_get_method(candidate_id):
 
         complete_url = URL + "candidates_list/" + str(candidate_id)
 
-        # sending get request and saving the response as response object 
-        r = requests.get(url = complete_url)
+        # sending get request and saving the response as response object
+        r = requests.get(url=complete_url)
 
-        # extracting data in json format 
+        # extracting data in json format
         data = r.json()
 
         if data == "Not Found":
@@ -66,18 +66,18 @@ def test_candidate_get_method(candidate_id):
 def test_candidate_put_method(candidate_id):
 
     if candidate_id is not None:
-        # data to be sent to api 
+        # data to be sent to api
         # data = {'name':'Ali',
         #         'age':'19',
         #         'language':'C#'}
-        data = {'language':'R'}  
-        
+        data = {'language': 'R'}
+
         complete_url = URL + "candidates_list/" + str(candidate_id)
-        
-        # sending post request and saving response as response object 
-        r = requests.put(url = complete_url, data = data) 
-        
-        # extracting response text  
+
+        # sending post request and saving response as response object
+        r = requests.put(url=complete_url, data=data)
+
+        # extracting response text
         updated_data = r.text
 
         if "Record not found" in updated_data:
@@ -87,7 +87,7 @@ def test_candidate_put_method(candidate_id):
         elif "New Created" in updated_data:
             print("New candidate id created successfully.")
         else:
-            print("The updated candidate parameters are:%s"%updated_data)
+            print("The updated candidate parameters are:%s" % updated_data)
 
 
 def test_candidate_delete_method(candidate_id):
@@ -95,7 +95,7 @@ def test_candidate_delete_method(candidate_id):
     if candidate_id is not None:
 
         complete_url = URL + "candidates_list/" + str(candidate_id)
-    
+
         r = requests.delete(url=complete_url)
 
         data = r.text
@@ -109,11 +109,13 @@ def test_candidate_delete_method(candidate_id):
 def main(args):
 
     if args.test_candidatelist_get is True:
-        print("--------------Testing CandidateList GET method is {}---------------".format(args.test_candidatelist_get))
+        print("--------------Testing CandidateList GET method is {}---------------".format(
+            args.test_candidatelist_get))
         test_candidatelist_get_method()
         print("---------------------------------------------------------------------")
     if args.test_candidatelist_post is True:
-        print("--------------Testing CandidateList POST method is {}--------------".format(args.test_candidatelist_post))
+        print("--------------Testing CandidateList POST method is {}--------------".format(
+            args.test_candidatelist_post))
         test_candidatelist_post_method()
         print("----------------------------------------------------------------------")
     if args.test_candidate_get is True:
@@ -134,12 +136,18 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--test_candidatelist_get", help="test get method", action='store_true', default='false')
-    parser.add_argument("--test_candidatelist_post", help="test post method", action='store_true', default='false')
-    parser.add_argument("--test_candidate_get", help="test get method", action='store_true', default='false')
-    parser.add_argument("--test_candidate_put", help="test put method", action='store_true', default='false')
-    parser.add_argument("--test_candidate_delete", help="test delete method", action='store_true', default='false')
-    parser.add_argument("--candidate_id", help="candidate id", type=int, default=None)
+    parser.add_argument("--test_candidatelist_get",
+                        help="test get method", action='store_true', default='false')
+    parser.add_argument("--test_candidatelist_post",
+                        help="test post method", action='store_true', default='false')
+    parser.add_argument("--test_candidate_get", help="test get method",
+                        action='store_true', default='false')
+    parser.add_argument("--test_candidate_put", help="test put method",
+                        action='store_true', default='false')
+    parser.add_argument("--test_candidate_delete",
+                        help="test delete method", action='store_true', default='false')
+    parser.add_argument(
+        "--candidate_id", help="candidate id", type=int, default=None)
 
     args = parser.parse_args()
 
---
+++ candidate.py
@@ -0,0 +1,76 @@
+class Candidate():
+
+    count = 1
+
+    def __init__(self, name, age, language):
+
+        self._name = name
+        self._age = age
+        self._language = language
+        self._id = Candidate.count
+
+        Candidate.count += 1
+
+
+# api = Api(app)
+
+# parser = reqparse.RequestParser()
+
+# class CanditatesList(Resource):
+#   def get(self):
+#       return CANDIDATES
+#   def post(self):
+#       parser.add_argument("name")
+#       parser.add_argument("age")
+#       parser.add_argument("language")
+#       args = parser.parse_args()
+#       candidate_id = int(max(CANDIDATES.keys())) + 1
+#       candidate_id = '%i' % candidate_id
+#       CANDIDATES[candidate_id] = {
+#           "name": args["name"],
+#           "age": args["age"],
+#           "language": args["language"],
+#       }
+#       return CANDIDATES[candidate_id], 201
+
+
+# class Candidate(Resource):
+#   def get(self, candidate_id):
+#     if candidate_id not in CANDIDATES:
+#       return "Not Found", 404
+#     else:
+#       return CANDIDATES[candidate_id]
+#   def put(self, candidate_id):
+#     parser.add_argument("name")
+#     parser.add_argument("age")
+#     parser.add_argument("language")
+#     args = parser.parse_args()
+#     if candidate_id not in CANDIDATES:
+#       if args['name'] is not None and args['age'] is not None and args['language'] is not None:
+#         CANDIDATES[candidate_id] = {}
+#         CANDIDATES[candidate_id]["name"] = args['name']
+#         CANDIDATES[candidate_id]["age"] = args['age']
+#         CANDIDATES[candidate_id]["language"] = args['language']
+#         return "New Created", 200
+#       else:
+#         return "Missing information", 404
+#     else:
+#       candidate = CANDIDATES[candidate_id]
+#       candidate["name"] = args['name'] if args["name"] is not None else candidate["name"]
+#       candidate["age"] = args['age'] if args["age"] is not None else candidate["age"]
+#       candidate["language"] = args['language'] if args["language"] is not None else candidate["language"]
+#       return candidate, 200
+#   def delete(self, candidate_id):
+#     if candidate_id not in CANDIDATES:
+#       return "Not Found", 404
+#     else:
+#       del CANDIDATES[candidate_id]
+#       return "Deleted", 204
+
+
+# api.add_resource(CanditatesList, '/candidates_list/')
+# api.add_resource(Candidate, '/candidates_list/<candidate_id>')
+
+
+# from IPython import embed
+# embed()
